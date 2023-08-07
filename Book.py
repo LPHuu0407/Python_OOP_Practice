833b13afafacd6485cf802d3824a5bcf17caa043
Le Phuoc Huu <lphuu04072003@gmail.com>
2023-08-07 11:02:10 +0700

    Repush Book.py

--- Book.py
+++ Book.py
@@ -13,31 +13,31 @@ class Book:
     def Get_BookID(self):
         return self.BookID
     def Set_BookID(self, value):
-        if type(value) is str:
+        if type(value) is str and len(value) != 0:
             self.BookID = value
         else:
-            print("Error BookID!")
+            print("ID of the book isn't number or empty!")
     def Get_NameBook(self):
         return self.NameBook
     def Set_NameBook(self, value):
-        if type(value) is str:
+        if type(value) is str and len(value) != 0:
             self.NameBook = value
         else:
-            print("Error NameBook!")
+            print("Name of the book isn't number or empty!")
     def Get_PriceBook(self):
         return self.PriceBook
     def Set_PriceBook(self, value):
-        if type(value) is float and value > 1000:
+        if type(value) is float and value >= 0:
             self.PriceBook = value
         else:
-            print("Error PriceBook!")
+            print("Price of the book isn't less than 0!")
     def Get_SaleBook(self):
         return self.SaleBook
     def Set_SaleBook(self, value):
-        if type(value) is float and value >= 0:
+        if type(value) is float and value > 0:
             self.SaleBook = value
         else:
-            print("Error SaleBook!")
+            print("Sale of the book isn't less than 0!")
     def Input_Information(self):
         value_BookID = str(input(f"Enter BookID: "))
         value_NameBook = str(input(f"Enter NameBook: "))
@@ -49,7 +49,7 @@ class Book:
         self.Set_SaleBook(value_SaleBook)
     def Output_Information(self):
         sale = self.Get_PriceBook() - self.Get_SaleBook()
-        return f" - ID of book: {self.Get_BookID()} \n - Name of book: {self.Get_NameBook()} \n - Price of book: {self.Get_PriceBook()}VND \n -Sale of book: {self.Get_SaleBook()}VND \n -Total: {sale}VND"
+        return f" - ID of the book: {self.Get_BookID()} \n - Name of the book: {self.Get_NameBook()} \n - Price of the book: {self.Get_PriceBook()}VND \n -Sale of the book: {self.Get_SaleBook()}VND \n -Total: {sale}VND"
     def Update_Information(self):
         self.Input_Information()
 print("Information current:")
