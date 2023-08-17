779c117d422c5f548505ce04768e785ee2c2789f
Le Phuoc Huu <lphuu04072003@gmail.com>
2023-08-17 20:00:29 +0700

    Repush edit Circle.py

--- Circle.py
+++ Circle.py
@@ -1,4 +1,5 @@
 import math
+import array as Arr
 class Circle:
     # Fields, Constructor
     def __init__(self, value_r : float = 1):
@@ -15,16 +16,23 @@ class Circle:
         else:
             self.r = Value
     def Input_r(self):
-        self.r = float(input("Enter the radius of the circle: "))
-        self.Set_r(self.r)
+        number_of_Circle = int(input("Enter number of the circle: "))
+        Circle_Array = Arr.array('f', [])
+        if number_of_Circle >= 1:
+            for i in range(0, number_of_Circle):
+                self.Set_r(float(input(f"Enter value of radius {i + 1}: ")))
+                Circle_Array.append(self.Get_r())
+                print(f"Radius recent input {i + 1}: {Circle_Array[i]}")
+        else:
+            print("Number of the circle isn't less than 0!")
+
+
+
+
+        
     def Output_r(self):
         return f" - Radius of the circle: {self.Get_r()}"
     def Area_of_Circle(self):
         return f" - Area of the circle: {math.pi * self.Get_r() ** 2}"
     def Perimeter_of_Circle(self):
         return f" - Perimeter of the circle: {2 * math.pi * self.Get_r()}"
-Circle_Object = Circle()
-Circle_Object.Input_r()
-print(Circle_Object.Output_r())
-print(Circle_Object.Area_of_Circle())
-print(Circle_Object.Perimeter_of_Circle())
